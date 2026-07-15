4896e7d46b94f72d18e13a7508e78305b4e12532
qwertyfox <rojeshresearch2@outlook.com>
2026-07-15 22:59:21 +1000

    ListRollOver codes

--- src/main/java/com/qwertyfox/logictest/ListRollOver.java
+++ src/main/java/com/qwertyfox/logictest/ListRollOver.java
@@ -13,14 +13,17 @@ public class ListRollOver {
 
     private static List<Integer> listA = Arrays.asList(1001, 3, 4, 5, 6, 7);
     private static List<Integer> listB = Arrays.asList(1002, 4, 5, 3, 4, 7);
-    private static List<Integer> listC = Arrays.asList(1002, 3, 6, 3, 4, 6);
+    private static List<Integer> listC = Arrays.asList(1003, 3, 6, 3, 4, 6);
 
 
     private static List<List<Integer>> computationalList = new ArrayList<>();
     private static Map<Integer, Integer> mapWithListTotal = new HashMap<>();
 
     public static void main(String[] args) {
+        addToMap();
+        calculateTotal();
 
+        System.out.println(mapWithListTotal);
     }
 
     // will be done by the Buffered reader
@@ -34,12 +37,17 @@ public class ListRollOver {
     private static void calculateTotal() {
         for(List<Integer> list: computationalList){
             int listSum = 0;
-            for(int i = 1; i <= list.size(); i ++) {
+            for(int i = 1; i < list.size(); i ++) {
                 listSum += list.get(i);
             }
             mapWithListTotal.put(list.get(0), listSum);
         }
     }
+
+    private static List<Integer> findList (int numberToAdd) {
+        return null;
+    }
+
 
     private static void addToList (int indexToAddFrom, int subElementToAddFrom, int nonZeroPositionNumberToAdd) {
 
@@ -49,7 +57,8 @@ public class ListRollOver {
         int subElement;
 
         while (true) {
-            // check if the total in the list a suffices to fulfil the numberToAdd criteria
+
+
         }
 
 
